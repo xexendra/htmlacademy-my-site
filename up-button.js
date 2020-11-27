fa91637ca0a2bd1bfd4f78bd063cb28c92e1b0d4
Varvara Kholmanskaya <70838206+xexendra@users.noreply.github.com>
2020-11-27 23:00:03 +0300

    добавлено меню, кнопка "Наверх", обновлены цвета

---
+++ up-button.js
@@ -0,0 +1,14 @@
+let upButton = document.querySelector('.up-button');
+
+window.onscroll = function () {
+  if (window.pageYOffset > 400) {
+    upButton.classList.add('shown');
+  } else {
+    upButton.classList.remove('shown');
+  }
+
+};
+
+upButton.onclick = function () {
+  window.scrollTo(0, 0);
+};
